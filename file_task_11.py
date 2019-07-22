d1b3a3c91fb328c2bb8778fe7093e08abc277b6c
Test Python <skuridin@gmail.com>
2019-07-22 18:55:26 +0300

    chars only

--- file_task_11.py
+++ file_task_11.py
@@ -1,29 +1,28 @@
 #with  open ('task1.txt') as inf:
 #   for line in inf:
-line ='n7b4z6t20'
-       #line = line.strip()
-       #print (line)
+line ='n7b4z6t20S6u16q15h4P1e5n3f12'
+      # line = line.strip()
+#print(line)
 i = 0
+#j = 0
 s =''
 d = dict()
+#line1 = line[i::-1]
 while i < len(line):
     #print(line[i], end ='')
-    if line[i].isdigit() == True:
-            #print(line[i],line[i:i+2],'i=',i, end= ' ')
-            print(line[i],i)
-            #s += str(line[i])
-            #if line[i:i+1].isdigit() == True:
-            #         print ('Два')
-            i += 1
+    if line[i].isdigit() != True:
+        print(line[i])
+            #if line[:i+1].isdigit() == True:
+             #   print(line[:i+1])
+        i += 1
     else:
-
-        print(line[i],i)
+        #print(line[i])
         i += 1
         #d[line[i]] = i
-
+    #i += 1
         #print (line[i:-1].lower())
         #print (line[i], line[i].isdigit())
         #print (line[i])
 #i += 1
-#print ()
+print (line)
 #print (d)
