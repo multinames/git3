0808c3def4b18d290dfd5b6baf237d988be83229
Test Python <skuridin@gmail.com>
2019-07-23 17:36:36 +0300

    Hm...1

--- file_task_11.py
+++ file_task_11.py
@@ -4,25 +4,28 @@ line ='n7b4z6t20S6u16q15h4P1e5n3f12'
       # line = line.strip()
 #print(line)
 i = 0
-#j = 0
+j = 0
 s =''
 d = dict()
-#line1 = line[i::-1]
+
 while i < len(line):
-    #print(line[i], end ='')
-    if line[i].isdigit() != True:
-        print(line[i])
-            #if line[:i+1].isdigit() == True:
-             #   print(line[:i+1])
-        i += 1
-    else:
-        #print(line[i])
-        i += 1
-        #d[line[i]] = i
-    #i += 1
-        #print (line[i:-1].lower())
-        #print (line[i], line[i].isdigit())
-        #print (line[i])
-#i += 1
-print (line)
-#print (d)
+    if line[i].isdigit() == True and line[i-1].isdigit() == True:
+            s += (line[i - 1] + line[i])
+            #print (line[i-1]+line[i], end= ' ')
+            print(line[i - 1] + line[i], end=' ')
+            i+=1
+    elif line[i].isdigit() == True and line[i+1].isdigit() != True:
+        j=0
+        s += (line[i])
+        print(line[i], end= ' ')
+
+    elif line[i].isalpha() == True:
+        j = 0
+        s +=line[i]
+        print(line[i], end= ' ')
+        d[line[i]]= 0
+    i+=1
+
+print(s)
+
+
