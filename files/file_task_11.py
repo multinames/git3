0dbf618246bab7d6946580570dcfb9aa0e3d71f8
Test Python <skuridin@gmail.com>
2019-07-24 18:56:46 +0300

    Last day

--- files/file_task_11.py
+++ files/file_task_11.py
@@ -5,19 +5,34 @@ line ='n7b4z6t20S6u16q15h4P1e5n3f12k1Y20B15v20i14P11a15m8r10m19S10h7N3w15a12X13v
 #print(line)
 i = 0
 #j = 0
-s =''
-d = dict()
-
+#s =''
+d = []
+print (len(line))
 while i < len(line):
-    if line[i].isdigit() == True and line[i-1].isdigit() == True:
-            print (line[i-1]+line[i],end=' ')
-    elif line[i].isdigit() == True and line[i+1].isdigit() != True:
+
+    #if line[i].isdigit() == True and line[i-1].isdigit() == True:
+     #       print (line[i-1]+line[i],end=' ')
+    #print(line[i-1], i, end=' ')
+    #print()
+    if line[i].isdigit() == True:
+            d.append (line[i])
             print(line[i], end=' ')
+
+    i += 1
+    #elif line[i].isdigit() == True:
+     #   print(line[i], end=' ')
+      #  n.add (line[i])
+       # i += 1
+
+print(d)
+            #print('i',line[i:i+1])
+    #elif line[i].isdigit() == True and line[i+1].isdigit() != True:
+     #       print(line[i], end=' ')
     #elif line[i].isdigit() != True:
      #       print(line[i], end=' ')
 
 
-    i+=1
+    #i+=1
     #print(d)
 
 
