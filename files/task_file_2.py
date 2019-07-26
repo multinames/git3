6c24af21666c6a782f7335909cf18b8cecb06664
skuridin@gmail.com <Ghbdtn00>
2019-07-26 23:49:50 +0300

    sort

--- files/task_file_2.py
+++ files/task_file_2.py
@@ -11,4 +11,15 @@ for i in s.split():
 		l[i] += 1
 	else:
 		l[i] = 1
-print (l)
+#print (l)
+
+#print({l[k]: k for k in l})
+#print(dict(reversed(item) for item in l.items()))
+#print (l.values())
+
+#for i in l.values():
+#	print (i)
+
+max_value = max(l.values())
+final_dict = {k: v for k, v in l.items() if v == max_value}
+print(final_dict)
