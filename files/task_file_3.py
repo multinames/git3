e2a2166b136412e6db89ad8bc9dad074df14c1eb
skuridin@gmail.com <Ghbdtn00>
2019-07-27 21:23:50 +0300

    average...

--- files/task_file_3.py
+++ files/task_file_3.py
@@ -1,14 +1,19 @@
 l = []
-d = dict()
-#i = 0
-s = 0
 with  open ('dataset_3363_4.txt') as inf:
    for a in inf :
     a = a.strip().split(';')
     l +=a
-    #print(a)
-print(l)
+#print(l)
+count, s = 0, 0
 for i in l:
-    #print(s)
-    if i.isalpha() != True:
-        print(i)
+    if i.isdigit() == True:
+            #print(i, end=' ')
+            s += int(i)
+            count += 1
+            if count == 3:
+             #   print()
+                print(s/3)
+                count = 0
+                s = 0
+
+
