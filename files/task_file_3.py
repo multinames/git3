a199e554d7677a141c9c18d0d65fcd380e2a8f3f
skuridin@gmail.com <Ghbdtn00>
2019-07-27 20:18:58 +0300

    more...

--- files/task_file_3.py
+++ files/task_file_3.py
@@ -7,39 +7,8 @@ with  open ('dataset_3363_4.txt') as inf:
     a = a.strip().split(';')
     l +=a
     #print(a)
-#print(l)
-d['marks'] = []
+print(l)
 for i in l:
     #print(s)
-    if i.isalpha() == True:
-        d['name'] = i
+    if i.isalpha() != True:
         print(i)
-        print(d)
-        #print(s)
-        #s = 0
-    #else:
-     #   while i.isdigit() == True:
-
-    elif i.isdigit() == True:
-
-        #s += int(i)
-        d['marks'].append(int(i))
-        print(i)
-            #s += int(i)
-
-            #break
-        #print(s)
-
-
-
-
-#l = {}
-#[print(el) for el in a]
-
-#for el in a:
-#    print(el)
-#for i in line:
-#	if i in l:
-#		l[i] += 1
-#	else:
-#		l[i] = 1
