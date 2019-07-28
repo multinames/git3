21cf7349f24be27ade54d35ccc7ae1ec846cc51c
skuridin@gmail.com <Ghbdtn00>
2019-07-29 00:19:28 +0300

    Finish 3 task file

--- files/task_files34.py
+++ files/task_files34.py
@@ -1,21 +1,14 @@
-l = []
 d = dict()
-i = 0
-s = 0
-n = 4
+s, s2 , s3 = 0, 0, 0
 with  open ('dataset_3363_4.txt') as inf:
    for a in inf :
     a = a.strip().split(';')
-    s = int(a[1]) + int(a[2])+int(a[3])
     d[a[0]] = [int(a[1]),int(a[2]),int(a[3])]
-    #print (a)
-    #print(a[0],s/3)
-#print(d)
+with open('outfile.txt', 'w') as ouf:
+    for value in d.values():
+        ouf.write(str(sum(value)/3)+'\n')
+        s += value[0]
+        s2 += value[1]
+        s3 += value[2]
+    ouf.write (str(s/(len(d))) +' '+ str(s2/(len(d))) + ' '+str(s3/(len(d))))
 
-for value in d.values():
-    print(sum(value)/3)
-
-#a = [[0] * n] * n
-#print(a)
-#a[0][0] = 5
-#print(a)
