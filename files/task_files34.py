efa5c18e2c03dde5ae084f2d905790e257d411dc
skuridin@gmail.com <Ghbdtn00>
2019-07-28 23:42:03 +0300

    dict

--- files/task_files34.py
+++ files/task_files34.py
@@ -1,13 +1,19 @@
 l = []
+d = dict()
 i = 0
 s = 0
 n = 4
 with  open ('dataset_3363_4.txt') as inf:
    for a in inf :
     a = a.strip().split(';')
-    #if a.isalpha() == True:
     s = int(a[1]) + int(a[2])+int(a[3])
-    print(a[0],s/3)
+    d[a[0]] = [int(a[1]),int(a[2]),int(a[3])]
+    #print (a)
+    #print(a[0],s/3)
+#print(d)
+
+for value in d.values():
+    print(sum(value)/3)
 
 #a = [[0] * n] * n
 #print(a)
