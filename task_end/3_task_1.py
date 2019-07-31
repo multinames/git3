1e1b9a68d598f0ec53c649f43c2713046806e1e5
Test Python <skuridin@gmail.com>
2019-07-31 16:52:39 +0300

    3 task more...

--- task_end/3_task_1.py
+++ task_end/3_task_1.py
@@ -1,9 +1,18 @@
 n = int(input())
 i = 0
-b = []
+b = dict()
 while i < n :
-    a = [i for i in input().split()]
-    print(a)
-    b += a
+    a = [i for i in input().split(';')]
+    print (a[1])
+    if a[1] in b:
+        #b[0] = +a[0]
+        print (a[3])
+        print ('Yes')
+    else:
+        print('No')
+        #print(a)
+    b[a[0]] = a[1]
+    print(b)
     i+=1
-print(b)
+
+
