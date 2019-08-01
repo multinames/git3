f1e39ebd3e80b9588ce487aaade7c0ace0a01a22
Test Python <skuridin@gmail.com>
2019-08-01 18:11:55 +0300

    22

--- task_end/3_task_1.py
+++ task_end/3_task_1.py
@@ -1,20 +1,24 @@
 n = int(input())
 i = 0
 b = dict()
+c = []
+#Команда:Всего_игр Побед Ничьих Поражений Всего_очков
+#За победу команде начисляется 3 очка, за поражение — 0, за ничью — 1.
 while i < n :
     j = 0
     a = [i for i in input().split(';')]
     while j < len(a):
         #print(a[j])
         if a[j].isalpha() == True:
-            print ('Команда :', a[j])
+            #print ('Команда :', a[j])
+            b[a[j]] = [0,0,0,0,0]
             j += 1
         else:
 
-            print(a[j])
+            #print(a[j])
 
             j += 1
     i+=1
-
+print (b)
 
 
