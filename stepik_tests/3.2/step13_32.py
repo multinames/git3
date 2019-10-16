40004b3604f05f42b29c91df54f84f8e2adb4646
Test Python <skuridin@gmail.com>
2019-10-16 14:27:56 +0300

    Stepik Tests

--- stepik_tests/3.2/step13_32.py
+++ stepik_tests/3.2/step13_32.py
@@ -6,29 +6,18 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_css_selector(".first_block .form-control.first")
+    input1 = browser.find_element_by_class("quiz__typename")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_xpath("//input[@placeholder='Input your last name']")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element_by_css_selector(".form-group.third_class .form-control.third")
-    input3.send_keys("test@test.com")
+
+    time.sleep(2)
 
     # send form
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element_by_class("submit-submission ")
     button.click()
 
     # check in registration
     # waiting for loader
     time.sleep(1)
-
-    # find elements
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
-
-    # write text
-    welcome_text = welcome_text_elt.text
-
-    # check in
-    assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
     # waiting
--- stepik_tests/3.5/test_task_run_1.py
+++ stepik_tests/3.5/test_task_run_1.py
@@ -5,12 +5,14 @@ class TestMainPage():
     # номер 1
     @pytest.mark.xfail
     @pytest.mark.smoke
-    def test_guest_can_login(self, browser):
+    def test_guest_can_login(self):
+        print ("RUN 1")
         assert True
 
     # номер 2
     @pytest.mark.regression
-    def test_guest_can_add_book_from_catalog_to_basket(self, browser):
+    def test_guest_can_add_book_from_catalog_to_basket(self):
+        print ("RUN 2")
         assert True
 
 
@@ -18,31 +20,37 @@ class TestBasket():
     # номер 3
     @pytest.mark.skip(reason="not implemented yet")
     @pytest.mark.smoke
-    def test_guest_can_go_to_payment_page(self, browser):
+    def test_guest_can_go_to_payment_page(self):
+        print ("RUN 3")
         assert True
 
     # номер 4
     @pytest.mark.smoke
-    def test_guest_can_see_total_price(self, browser):
+    def test_guest_can_see_total_price(self):
+        print ("RUN 4")
         assert True
 
 
 @pytest.mark.skip
 class TestBookPage():
+
     # номер 5
     @pytest.mark.smoke
-    def test_guest_can_add_book_to_basket(self, browser):
+    def test_guest_can_add_book_to_basket(self):
+        print ("RUN 5")
         assert True
 
     # номер 6
     @pytest.mark.regression
-    def test_guest_can_see_book_price(self, browser):
+    def test_guest_can_see_book_price(self):
+        print ("RUN 6")
         assert True
 
 
-# номер 7
-@pytest.mark.beta_users
-@pytest.mark.smoke
-def test_guest_can_open_gadget_catalogue(browser):
-    assert True
+    # номер 7
+    @pytest.mark.beta_users
+    @pytest.mark.smoke
+    def test_guest_can_open_gadget_catalogue(self):
+        print ("RUN 7")
+        assert True
 
