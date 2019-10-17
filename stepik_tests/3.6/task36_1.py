cc223afe3285436439d341710be9cb4fc8c5366e
Test Python <skuridin@gmail.com>
2019-10-17 15:37:40 +0300

    Tasks for 3.4

--- stepik_tests/3.6/task36_1.py
+++ stepik_tests/3.6/task36_1.py
@@ -1,9 +1,7 @@
 ﻿from selenium import webdriver
 import time
 import math
-#import pytest
-
-answer = math.log(int(time.time()))
+import pytest
 
 try:
     link = "http://stepik.org/lesson/236895/step/1/"
@@ -14,22 +12,19 @@ try:
     answer = math.log(int(time.time()))
 #    print ("PRINT!",answer)
 
-    input1 = browser.find_element_by_css_selector(".textarea")
+#    
 #    input1 = browser.find_elements_by_tag_name('textarea')
-#    input1 =  browser.find_element_by_xpath ("//textarea")
-    input1.send_keys(answer)
-    time.sleep(3)
+    input1 =  browser.find_element_by_xpath ("//textarea")
+    input1.send_keys(str(answer))
 
     # send form
     button = browser.find_element_by_class_name("submit-submission")
     button.click()
 
-    # check in registration
-    # waiting for loader
     time.sleep(1)
 
 finally:
     # waiting
-    time.sleep(10)
+    time.sleep(5)
     # close 
-    browser.quit()
+#    browser.quit()
--- stepik_tests/3.6/task36_11.py
+++ stepik_tests/3.6/task36_11.py
@@ -6,20 +6,34 @@ from selenium import webdriver
 
 @pytest.fixture(scope="function")
 def browser():
-    print("\nstart browser for test..")
+#    print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    time.sleep(5)
-    print("\nquit browser..")
+#    print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('url', ["stepik.org/lesson/236895/step/1"])
-def test_guest_should_see_login_link(browser, url):
-    answer = math.log(int(time.time()))
-    print(answer)
+@pytest.mark.parametrize('url', ["stepik.org/lesson/236895/step/1","stepik.org/lesson/236896/step/1","stepik.org/lesson/236897/step/1","stepik.org/lesson/236898/step/1","stepik.org/lesson/236899/step/1","stepik.org/lesson/236903/step/1","stepik.org/lesson/236904/step/1","stepik.org/lesson/236905/step/1"])
+def test_ufo(browser, url):
+
     link = f"https://{url}/"
+    browser.implicitly_wait(5)
     browser.get(link)
-#    browser.find_element_by_css_selector("ember1598")
-    input1 = browser.find_element_by_css_selector("quiz-component.ember-view")
-    input1.send_keys(answer)
-    
+
+    input1 =  browser.find_element_by_xpath ("//textarea")
+    answer = math.log(int(time.time()))
+    input1.send_keys(str(answer))
+
+    # send form
+    button = browser.find_element_by_class_name("submit-submission")
+    button.click()
+
+    browser.implicitly_wait(3)
+    correct = browser.find_element_by_css_selector(".smart-hints__hint")
+#    correct = browser.find_element_by_xpath ("//pre[text()='Correct!']")
+    browser.implicitly_wait(3)
+    msg =  correct.text
+    correct_msg = "Correct!"
+    assert correct_msg == msg
+#    print (msg)
+
+#    time.sleep(2)
