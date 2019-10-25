e714a7c04e40d6453d4c850154e4ad567103c7d2
Test Python <skuridin@gmail.com>
2019-10-25 15:07:40 +0300

    test oop

---
+++ python_basis/1.2/task1.2_6.py
@@ -0,0 +1,4 @@
+x = [1,2,3]
+type(x)
+type(x)
+type(type(x))
---
+++ tests_users/kiras96/test_items.py
@@ -0,0 +1,17 @@
+﻿import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_find_button(browser):
+    browser.get(link)
+    find_button = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket"))
+    )
+    browser.execute_script("return arguments[0].scrollIntoView({block: 'center'});", find_button)
+
+    assert bool(find_button) is True, "Button not found"
+    time.sleep(10)
