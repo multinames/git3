dc6760df50af79f78be77c5ff6940636547bd642
skuridin@gmail.com <Ghbdtn00>
2019-12-04 18:07:54 +0300

    add first example file

--- page_object/pages/base_page.py
+++ page_object/pages/base_page.py
@@ -1,8 +1,8 @@
 ﻿class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
+    def __init__(self, browser, url):    #метод (конструктор __init__)
+        self.browser = browser           #экземпляр драйвера
+        self.url = url                   #URL
         self.timeout = 5
     
-    def open(self):
+    def open(self):                      #метод
         self.browser.get(self.url)
